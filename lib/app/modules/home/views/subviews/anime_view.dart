26a9c3cf42c54e8a04ce91ec8ce9d6f71562d689
niconikx <nikilmrz2060@gmail.com>
2024-09-25 08:33:18 +0545

    init

--- lib/app/modules/home/views/subviews/anime_view.dart
+++ lib/app/modules/home/views/subviews/anime_view.dart
@@ -1,3 +1,5 @@
+import 'package:anipiece/app/core/values/s_spacing.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -7,16 +9,79 @@ class AnimeView extends GetView {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('AnimeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'AnimeView is working',
-          style: TextStyle(fontSize: 20),
+      body:Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CarouselSlider(
+  options: CarouselOptions(height: 400.0, autoPlay: true),
+  items: [1,2,3,4,5].map((i) {
+    return Builder(
+      builder: (BuildContext context) {
+        return Container(
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
+          decoration: BoxDecoration(
+            color: Colors.amber
+          ),
+          child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+        );
+      },
+    );
+  }).toList(),
+),
+              Row(
+                children: [
+                  SSpacing.smW,
+                  Text("Popular Anime",
+                  style: Get.textTheme.titleLarge,
+                  ),
+                  const Spacer(),
+                  Text('See all',
+                  style: Get.textTheme.bodyMedium,
+                  ),
+                  Icon(Icons.arrow_forward_ios,size: 10,),
+                  SSpacing.smW,
+                ],
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+             child:  Row(
+                children: [
+                  SSpacing.smW,
+                  Container(
+                    width: 150,
+                    height: 200,
+                    color: Colors.amberAccent,
+                  ),
+                  SSpacing.smW,
+                  Container(
+                    width: 150,
+                    height: 200,
+                    color: Colors.amberAccent,
+                  ),
+                  SSpacing.smW,
+                  Container(
+                    width: 150,
+                    height: 200,
+                    color: Colors.amberAccent,
+                  ),
+                  SSpacing.smW,
+                     Container(
+                    width: 150,
+                    height: 200,
+                    color: Colors.amberAccent,
+                  ),
+                  SSpacing.smW,
+               
+                ],
+              ),
+              ),
+            ],
+          ),
         ),
-      ),
+      )
+
     );
   }
 }
